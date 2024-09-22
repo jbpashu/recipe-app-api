811da9cf1df833826a3934ddc6a7ccba0e7e8cb1
Ashish Awasthy <ashish.awasthy@tech9.com>
2024-09-23 00:14:11 +0530

    Adding models

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -3,7 +3,7 @@ Test custom Django management commands.
 """
 from unittest.mock import patch
 
-from psycopg2 import OperationalError as Psycopg2Error
+from psycopg2 import OperationalError as Psycopg2OpError
 
 from django.core.management import call_command
 from django.db.utils import OperationalError
@@ -11,11 +11,11 @@ from django.test import SimpleTestCase
 
 
 @patch('core.management.commands.wait_for_db.Command.check')
-class CommandTest(SimpleTestCase):
-    """Test Commands."""
+class CommandTests(SimpleTestCase):
+    """Test commands."""
 
     def test_wait_for_db_ready(self, patched_check):
-        """ Test waiting for database if database ready."""
+        """Test waiting for database if database ready."""
         patched_check.return_value = True
 
         call_command('wait_for_db')
@@ -24,8 +24,8 @@ class CommandTest(SimpleTestCase):
 
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
-        """ Test waiting fir databse when getting OperationnnError."""
-        patched_check.side_effect = [Psycopg2Error] * 2 + \
+        """Test waiting for databse when getting OperationalError."""
+        patched_check.side_effect = [Psycopg2OpError] * 2 + \
             [OperationalError] * 3 + [True]
 
         call_command('wait_for_db')
