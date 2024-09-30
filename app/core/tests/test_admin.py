b3e73f529de6a7a299506d1e99ad999883d9f5cc
Ashish Awasthy <ashish.awasthy@tech9.com>
2024-09-30 12:11:59 +0530

    Adding user service

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -7,10 +7,11 @@ from django.contrib.auth import get_user_model
 from django.urls import reverse
 from django.test import Client
 
+
 class AdminSiteTests(TestCase):
     """Tests for Django admin."""
 
-    def SetUp(self):
+    def setUp(self):
         """Create user and client."""
         self.client = Client()
         self.admin_user = get_user_model().objects.create_superuser(
@@ -23,11 +24,25 @@ class AdminSiteTests(TestCase):
             password='testpass123',
             name='Test User'
         )
-    
-    def test_users_list(self):
+
+    def test_users_lists(self):
         """Test that users are listed on page."""
         url = reverse('admin:core_user_changelist')
         res = self.client.get(url)
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_edit_user_page(self):
+        """Test the edit user page works."""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """Test the Ccreate user page works."""
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
