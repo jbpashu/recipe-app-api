6c7cd141ab9e5ee0c68ca7bd37cf0302084ba75d
Ashish Awasthy <ashish.awasthy@tech9.com>
2024-10-08 13:54:09 +0530

    Adding code for User API

--- app/user/views.py
+++ app/user/views.py
@@ -2,13 +2,33 @@
 Views for the user API.
 """
 
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
-from user.serializers import UserSerializer
+from user.serializers import (
+    UserSerializer,
+    AuthTokenSerializer,
+)
 
 from django.shortcuts import render
 
 
-class CreateUserView(generics.CreateAPiView):
-    """Create a new uuser in the system"""
-    seralizer_class = UserSerializer
+class CreateUserView(generics.ListCreateAPIView):
+    """Create a new uuser in the system."""
+    serializer_class = UserSerializer
+
+class CreateTokenView(ObtainAuthToken):
+    """Creata a new auith token for user."""
+    serialier_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authenticated user."""
+    serializer_class = UserSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        """Retrieve and return the authenticated user."""
+        return self.request.user
